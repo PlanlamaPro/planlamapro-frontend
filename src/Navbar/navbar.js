eb793743788f2a07a66fffcf1808772cc42a62db
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2023-05-12 15:59:02 +0300

    Responsi

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -4,11 +4,16 @@ import './navbar.css';
 import logo from '../Logo/planlamaproLogo.png';
 import logo2 from '../Logo/planlamaproLogoMini.png';
 import React, { useState, useEffect } from "react";
+//Son Eklenen
+import icon from '../Logo/button_icon.png';
 
 function App() {
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [showText, setShowText] = useState(true);
+  //yeni eklenti
+  const [changeNav, setChangeNav] = useState(true);
+  //Bitiş
 
   useEffect(() => {
     const handleResize = () => setWindowWidth(window.innerWidth);
@@ -27,41 +32,119 @@ function App() {
 
   let logoSrc = windowWidth < 785 ? logo : logo2;
 
+  //Son Eklemeler
+  useEffect(() => {
+    if (windowWidth < 655) {
+      setChangeNav(false);
+    }
+    else {
+      setChangeNav(true);
+    }
+  }, [windowWidth]);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+  //Son Eklemeler
+
 
   return (  
-    <Router>   
-      <div className='router'>
+    <Router> 
+
+
+      
+      {changeNav == true ? (
+        <div className='router'>
         <div className='logodiv'>
           <img className='logo' src={logoSrc} alt="Logo" />
           {showText && <h4 className='logoName'>PlanlamaPro</h4>}
         </div>
-
-        <div className='navbar'>
-          <nav>
-            <ul className='navbar-list' style={{ listStyleType: "none" }}>
-              <li>
-                <Link to="/" className='link'>Anasayfa</Link>
-              </li>
-              <li>
-                <Link to="/hakkimizda" className='link'>Hakkımızda</Link>
-              </li>
-              <li>
-                <Link to="/roomCreate" className='link'>Oda Oluştur</Link>
-              </li>
-              <li>
-                <Link to="/myRooms" className='link'>Odalarım</Link>
-              </li>
-              <li>
-                <Link to="/profile" className='link'>Profil</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
+      
+        
+          <div className='navbar'>
+            <nav>
+              <ul className='navbar-list'>
+                <li>
+                  <Link to="/" className='link'>Anasayfa</Link>
+                </li>
+                <li>
+                  <Link to="/hakkimizda" className='link'>Hakkımızda</Link>
+                </li>
+                <li>
+                  <Link to="/roomCreate" className='link'>Oda Oluştur</Link>
+                </li>
+                <li>
+                  <Link to="/myRooms" className='link'>Odalarım</Link>
+                </li>
+                <li>
+                  <Link to="/profile" className='link'>Profil</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          
+        
+       
         <div className='user'>
           <h4>Kullanıcı Adı</h4>
         </div>
+        
+          
+        
+        
+
+
       </div>
+      ): (
+        <div className='navbarMobileEngine'>
+          <div className='navbarMobile'>
+            <div className='logodiv'>
+              <img className='logo' src={logo2} alt="Logo" />
+            </div>
+            <div>
+              <h4 className='logoName'>PlanlamaPro</h4>
+            </div>
+            <div className='buttonDiv'>
+              <button className='windowButton' onClick={toggleModal}><img className='buttonIcon' src={icon} /></button>
+            </div>
+          </div>
+          {isOpen && (
+          <div className="modal">
+            
+                  <div className='modalNavbar'>
+                    <nav>
+                      <ul className='modal-list'>
+                        <li>
+                          <Link to="/" className='modal-link'>Anasayfa</Link>
+                        </li>
+                        <li>
+                          <Link to="/hakkimizda" className='modal-link'>Hakkımızda</Link>
+                        </li>
+                        <li>
+                          <Link to="/roomCreate" className='modal-link'>Oda Oluştur</Link>
+                        </li>
+                        <li>
+                          <Link to="/myRooms" className='modal-link'>Odalarım</Link>
+                        </li>
+                        <li>
+                          <Link to="/profile" className='modal-link'>Profil</Link>
+                        </li>
+                        <div className='modal-user'>
+                          <h4>Kullanıcı Adı</h4>
+                        </div>
+                      </ul>
+                    </nav>
+                  </div>
+                  
+              
+          </div>
+          )}
+        </div>
+      )}  
+      
+      
 
       <div className='routes'> 
         <div className='route'>
